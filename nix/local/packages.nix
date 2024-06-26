15b86da8814341f5010c45ef9d174ada988b3938
whs <hswongac@gmail.com>
2024-06-26 18:50:10 +0800

    Update

--- nix/local/packages.nix
+++ nix/local/packages.nix
@@ -9,6 +9,17 @@ with inputs.nixpkgs; let
     vendorHash = "sha256-+kxgV2YaKzNIEYZDAuT+H5iEeFU2f7/AMdYlyVl4jIg=";
     subPackages = "cmd/iris";
   };
+  iris_3_1_0 = buildGoModule rec {
+    pname = "iris";
+    version = "3.1.0";
+    src = fetchurl {
+      sha256 = "sha256-SwdRGk30QhZUoaIdTOk1+RCGZzJ6Dw3YzCY79smOtEQ=";
+      url = "https://github.com/irisnet/irishub/archive/refs/tags/v${version}.tar.gz";
+    };
+    vendorHash = "sha256-+0CnMC4sMyolmAkrpjBf/G4XwWZyKHBN06FAC5GiHkE=";
+    subPackages = "cmd/iris";
+  };
 in {
   inherit iris_3_0_0;
+  inherit iris_3_1_0;
 }
